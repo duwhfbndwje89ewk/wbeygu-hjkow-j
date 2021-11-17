45c325636560835ce7fbc5e6baf63033657e3433
duwhfbndwje89ewk <94533264+duwhfbndwje89ewk@users.noreply.github.com>
2021-11-17 14:22:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+const ayarlar = require("./ayarlar.json");
 
-client.login(process.env.token);
+client.login(ayarlar.token);
 
 client.on("ready", async () => {
 client.channels.get("906984707382734932").join()
